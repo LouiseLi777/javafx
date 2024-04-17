338fb25229b036c1622a50091825330480cdc7ba
LouiseLi777 <xl435@duke.edu>
2024-04-17 02:17:20 -0400

    update to 3.4

--- app/src/main/java/edu/duke/ece651/xl435/calculator/App.java
+++ app/src/main/java/edu/duke/ece651/xl435/calculator/App.java
@@ -3,20 +3,56 @@
  */
 package edu.duke.ece651.xl435.calculator;
 
+import java.io.IOException;
+import java.net.URL;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 public class App extends Application {
     @Override
-    public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion +
-                            ", running on Java " +
-                            javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
+    public void start(Stage stage) throws IOException {
+        // GridPane gp = new GridPane();
+        // for(int i =0;i<4;i++){
+        //     ColumnConstraints cc = new ColumnConstraints();
+        //     cc.setPercentWidth(25);
+        //     gp.getColumnConstraints().add(cc);
+        // }
+        // for(int i =0;i<5;i++){
+        //     RowConstraints rc = new RowConstraints();
+        //     rc.setPercentHeight(20);
+        //     gp.getRowConstraints().add(rc);
+        // }
+        // String[] labels = new String[]{"+","-","*","/",
+        // "7","8","9","4","5","6","1","2","3","."};
+        // int rows[] = new int[]{0,0,0,0,1,1,1,2,2,2,3,3,3,4};
+        // int cols[] = new int[]{0,1,2,3,0,1,2,0,1,2,0,1,2,2};
+        // for(int i =0;i<labels.length;i++){
+        //     Button b = new Button(labels[i]);
+        //     gp.add(b,cols[i],rows[i]);
+        //     b.setMaxWidth(Double.MAX_VALUE);
+        //     b.setMaxHeight(Double.MAX_VALUE);
+        // }
+        // Button b = new Button("0");
+        // b.setMaxWidth(Double.MAX_VALUE);
+        // b.setMaxHeight(Double.MAX_VALUE);
+        // gp.add(b,0,4,2,1);
+        // b = new Button("E\nn\nt\ne\nr");
+        // b.setMaxWidth(Double.MAX_VALUE);
+        // b.setMaxHeight(Double.MAX_VALUE);
+        // gp.add(b, 3,1,1,3);
+        URL xmlResource = getClass().getResource("/ui/calc-split.xml");
+        URL cssResource = getClass().getResource("/ui/calcbuttons.css");
+        GridPane gp = FXMLLoader.load(xmlResource);
+        Scene scene = new Scene(gp, 640, 480);
+        scene.getStylesheets().add(cssResource.toString());
         stage.setScene(scene);
         stage.show();
     }
